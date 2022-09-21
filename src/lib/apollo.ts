9ac83c24bd03b77077b5c29e997ff4d7d26e8f79
Nadi Duno <nadiduno@gmail.com>
2022-09-20 21:10:50 -0300

    Variavel desenvolvimento

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.hygraph.com/v2/cl4qksaa53yoy01xt4mrbacib/master',
+  uri:import.meta.env.VITE_API_URL,
   cache:new InMemoryCache()
 })
